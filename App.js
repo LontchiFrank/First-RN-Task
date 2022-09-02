82eacd26f16f6bda681fcd0fdd09e2df7a382c24
LontchiFrank <fodjof6@gmail.com>
2022-09-02 13:19:23 +0100

    my first react native app

--- App.js
+++ App.js
@@ -1,20 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  Pressable,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [enteredText, setEnteredText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredGoal) => {
+    setEnteredText(enteredGoal);
+  };
+
+  const addGoalEntered = () => {
+    // console.log(enteredText);
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      { key: Math.random().toString(), value: enteredText },
+    ]);
+    console.log(courseGoals);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Enter Text here"
+          style={{ width: "80%", borderColor: "black", borderWidth: 1 }}
+          onChangeText={goalInputHandler}
+          value={enteredText}
+        />
+        <Pressable style={styles.button} onPress={addGoalEntered}>
+          <Text style={styles.text}>save</Text>
+        </Pressable>
+      </View>
+      <FlatList
+        keyExtractor={(item, index) => item.key}
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <View style={styles.textContainer}>
+            <Text style={styles.text1}>{itemData.item.value} </Text>
+          </View>
+        )}
+      />
+      {/* List of items */}
+      {/* {courseGoals.map((goal) => (
+         
+        ))} */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 70,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  button: {
+    width: 50,
+    height: 30,
+    backgroundColor: "#450a40",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    // color: "#fff",
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+  text1: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "black",
+  },
+
+  textContainer: {
+    width: 200,
+    height: 50,
+    borderRadius: 5,
+    backgroundColor: "#f2a9ec",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 13,
   },
 });
